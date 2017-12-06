bf51d7a9862537833afad89963af3e38e435b09e
Hitkul <hitkuljangid@gmail.com>
2017-12-06 18:14:34 +0530

    scraping done

--- scraper.py
+++ scraper.py
@@ -1,25 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-
+id = 0
 def spider(year):
+    global id
+    movie_details = {}
     page = 1
-    max_pages = float('Inf')
+    max_pages = 2
     while page <= max_pages:
         print(page)
         url = 'http://www.imdb.com/search/title?release_date='+str(year)+'&page='+str(page)+'&ref_=adv_nxt'
         source_code = requests.get(url, allow_redirects=False)
         soup = BeautifulSoup(source_code.text,'html.parser')
-        page_number = soup.find('div',class_ = 'desc')
-        page_number_text = page_number.text
-        max_pages = int(page_number_text[page_number_text.index("of ")+3:page_number_text.index("titles")].replace(",",""))
         movies = soup.find_all('div', class_ = 'lister-item mode-advanced')
         for movie in movies:
             name = movie.h3.a.text
             descrption = movie.find_all('p',class_ = 'text-muted')[1].text
-            print(name)
-            print(descrption)
-            print("--------------------")
+            movie_details[id]= {'name':name,'year':year,'plot':descrption}
+            id+=1
+        page+=1
+    print(movie_details)
 
 
 
